cfe2eb4b53b13a674b8a2e6fa07e539d0fc68836
Humberto Cervantes <humberto.cervantes@gmail.com>
2025-05-29 13:21:32 -0600

    Added terraform configuration

---
+++ terraform-k8s/variables.tf
@@ -0,0 +1,5 @@
+variable "image" {
+  description = "Imagen Docker a desplegar"
+  type        = string
+  default     = "ghcr.io/ingsoftuami/devopstoolsis:latest" # reemplaza esto con tu imagen real
+}
