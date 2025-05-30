0703614661630bf4daed2317643cc2c6bf8c13b7
Humberto Cervantes <humberto.cervantes@gmail.com>
2025-05-29 20:43:50 -0600

    Working version

--- terraform-k8s/variables.tf
+++ terraform-k8s/variables.tf
@@ -1,5 +1,5 @@
 variable "image" {
   description = "Imagen Docker a desplegar"
   type        = string
-  default     = "ghcr.io/ingsoftuami/devopstoolsis:latest" # reemplaza esto con tu imagen real
+  default     = "ghcr.io/ingsoftuami/devopstoolsis:sha-57f9a7e" # Updated to latest commit with actuator
 }
